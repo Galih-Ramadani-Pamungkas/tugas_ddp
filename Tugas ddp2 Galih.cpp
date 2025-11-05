6eb4a183de40a6de44f837ffd826081026dfd341
Galih-Ramadani-Pamungkas <2500018047@webmail.uad.ac.id>
2025-11-05 10:49:59 +0700

    Update Tugas ddp2 Galih.cpp

--- Tugas ddp2 Galih.cpp
+++ Tugas ddp2 Galih.cpp
@@ -10,16 +10,17 @@ int main() {
 	cout <<"Masukan jumlah detik yang kamu inginkan:";
 	cin >> dtk;
 	
-    hari=dtk/86400;
-	jam=(dtk%86400)/3600;
-	menit=(dtk%3600)/60;
-	jumdet=dtk%60;
+    hari= dtk/86400;
+	jam= dtk % 86400/3600;
+	menit= dtk % 3600/60;
+	jumdet= dtk % 60;
 	
-	cout << endl;
-	cout << "hasil konversi:" <<endl;
-	cout << dtk << " " << "detik" << " " << "=" << hari << "hari" <<"-"<< jam <<"jam"<< "-" << menit <<"menit"<<"-" << jumdet <<"detik";
+	cout << "hasil konversi :  " << dtk << " " << "detik" << " " << "=" << " " << hari << "hari" <<"-"<< jam <<"jam"<< "-" << menit <<"menit"<<"-" << jumdet <<"detik";
+	
+	return 0;
     
 }
+
 
 
 
